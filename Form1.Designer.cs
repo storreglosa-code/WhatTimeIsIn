a05fcc353c85c8825fb2c4d0e2996f9024044422
Santiago Torreglosa <santiago.torreglosa@gmail.com>
2025-02-12 08:35:25 -0300

    UI/UX terminado. Listo para empezar con la logica

---
+++ Form1.Designer.cs
@@ -0,0 +1,399 @@
+﻿namespace WhatTimeIsIn
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            lblTime1 = new Label();
+            lblCity1 = new Label();
+            lblTicks1 = new Label();
+            lblDate1 = new Label();
+            lblDay1 = new Label();
+            panel2 = new Panel();
+            lblDay2 = new Label();
+            lblDate2 = new Label();
+            lblTicks2 = new Label();
+            lblTime2 = new Label();
+            lblCity2 = new Label();
+            panel3 = new Panel();
+            lblDay3 = new Label();
+            lblDate3 = new Label();
+            lblTicks3 = new Label();
+            lblTime3 = new Label();
+            lblCity3 = new Label();
+            panel4 = new Panel();
+            lblDay4 = new Label();
+            lblDate4 = new Label();
+            lblTicks4 = new Label();
+            lblTime4 = new Label();
+            lblCity4 = new Label();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            panel3.SuspendLayout();
+            panel4.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BorderStyle = BorderStyle.Fixed3D;
+            panel1.Controls.Add(lblDay1);
+            panel1.Controls.Add(lblDate1);
+            panel1.Controls.Add(lblTicks1);
+            panel1.Controls.Add(lblTime1);
+            panel1.Controls.Add(lblCity1);
+            panel1.Location = new Point(34, 22);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(489, 294);
+            panel1.TabIndex = 0;
+            // 
+            // lblTime1
+            // 
+            lblTime1.AutoSize = true;
+            lblTime1.BorderStyle = BorderStyle.Fixed3D;
+            lblTime1.Font = new Font("DS-Digital", 72F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblTime1.ForeColor = SystemColors.HighlightText;
+            lblTime1.Location = new Point(87, 80);
+            lblTime1.Name = "lblTime1";
+            lblTime1.Size = new Size(229, 97);
+            lblTime1.TabIndex = 1;
+            lblTime1.Text = "08:15";
+            // 
+            // lblCity1
+            // 
+            lblCity1.AutoSize = true;
+            lblCity1.Font = new Font("Rockwell", 26.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblCity1.ForeColor = SystemColors.HighlightText;
+            lblCity1.Location = new Point(121, 18);
+            lblCity1.Name = "lblCity1";
+            lblCity1.Size = new Size(227, 39);
+            lblCity1.TabIndex = 0;
+            lblCity1.Text = "Buenos Aires";
+            // 
+            // lblTicks1
+            // 
+            lblTicks1.AutoSize = true;
+            lblTicks1.BorderStyle = BorderStyle.Fixed3D;
+            lblTicks1.Font = new Font("DS-Digital", 36F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblTicks1.ForeColor = SystemColors.HighlightText;
+            lblTicks1.Location = new Point(308, 102);
+            lblTicks1.Name = "lblTicks1";
+            lblTicks1.Size = new Size(70, 49);
+            lblTicks1.TabIndex = 2;
+            lblTicks1.Text = "30";
+            // 
+            // lblDate1
+            // 
+            lblDate1.AutoSize = true;
+            lblDate1.BorderStyle = BorderStyle.Fixed3D;
+            lblDate1.Font = new Font("DS-Digital", 36F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblDate1.ForeColor = SystemColors.HighlightText;
+            lblDate1.Location = new Point(16, 214);
+            lblDate1.Name = "lblDate1";
+            lblDate1.Size = new Size(249, 49);
+            lblDate1.TabIndex = 3;
+            lblDate1.Text = "Feb 12 2025";
+            // 
+            // lblDay1
+            // 
+            lblDay1.AutoSize = true;
+            lblDay1.BorderStyle = BorderStyle.Fixed3D;
+            lblDay1.Font = new Font("DS-Digital", 27.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblDay1.ForeColor = SystemColors.HighlightText;
+            lblDay1.Location = new Point(293, 225);
+            lblDay1.Name = "lblDay1";
+            lblDay1.Size = new Size(179, 38);
+            lblDay1.TabIndex = 4;
+            lblDay1.Text = "Wednesday";
+            // 
+            // panel2
+            // 
+            panel2.BorderStyle = BorderStyle.Fixed3D;
+            panel2.Controls.Add(lblDay2);
+            panel2.Controls.Add(lblDate2);
+            panel2.Controls.Add(lblTicks2);
+            panel2.Controls.Add(lblTime2);
+            panel2.Controls.Add(lblCity2);
+            panel2.Location = new Point(561, 22);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(489, 294);
+            panel2.TabIndex = 1;
+            // 
+            // lblDay2
+            // 
+            lblDay2.AutoSize = true;
+            lblDay2.BorderStyle = BorderStyle.Fixed3D;
+            lblDay2.Font = new Font("DS-Digital", 27.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblDay2.ForeColor = SystemColors.HighlightText;
+            lblDay2.Location = new Point(293, 225);
+            lblDay2.Name = "lblDay2";
+            lblDay2.Size = new Size(179, 38);
+            lblDay2.TabIndex = 4;
+            lblDay2.Text = "Wednesday";
+            // 
+            // lblDate2
+            // 
+            lblDate2.AutoSize = true;
+            lblDate2.BorderStyle = BorderStyle.Fixed3D;
+            lblDate2.Font = new Font("DS-Digital", 36F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblDate2.ForeColor = SystemColors.HighlightText;
+            lblDate2.Location = new Point(16, 214);
+            lblDate2.Name = "lblDate2";
+            lblDate2.Size = new Size(249, 49);
+            lblDate2.TabIndex = 3;
+            lblDate2.Text = "Feb 12 2025";
+            // 
+            // lblTicks2
+            // 
+            lblTicks2.AutoSize = true;
+            lblTicks2.BorderStyle = BorderStyle.Fixed3D;
+            lblTicks2.Font = new Font("DS-Digital", 36F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblTicks2.ForeColor = SystemColors.HighlightText;
+            lblTicks2.Location = new Point(308, 102);
+            lblTicks2.Name = "lblTicks2";
+            lblTicks2.Size = new Size(70, 49);
+            lblTicks2.TabIndex = 2;
+            lblTicks2.Text = "30";
+            // 
+            // lblTime2
+            // 
+            lblTime2.AutoSize = true;
+            lblTime2.BorderStyle = BorderStyle.Fixed3D;
+            lblTime2.Font = new Font("DS-Digital", 72F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblTime2.ForeColor = SystemColors.HighlightText;
+            lblTime2.Location = new Point(87, 80);
+            lblTime2.Name = "lblTime2";
+            lblTime2.Size = new Size(229, 97);
+            lblTime2.TabIndex = 1;
+            lblTime2.Text = "08:15";
+            // 
+            // lblCity2
+            // 
+            lblCity2.AutoSize = true;
+            lblCity2.Font = new Font("Rockwell", 26.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblCity2.ForeColor = SystemColors.HighlightText;
+            lblCity2.Location = new Point(188, 18);
+            lblCity2.Name = "lblCity2";
+            lblCity2.Size = new Size(128, 39);
+            lblCity2.TabIndex = 0;
+            lblCity2.Text = "Sidney";
+            // 
+            // panel3
+            // 
+            panel3.BorderStyle = BorderStyle.Fixed3D;
+            panel3.Controls.Add(lblDay3);
+            panel3.Controls.Add(lblDate3);
+            panel3.Controls.Add(lblTicks3);
+            panel3.Controls.Add(lblTime3);
+            panel3.Controls.Add(lblCity3);
+            panel3.Location = new Point(34, 339);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(489, 294);
+            panel3.TabIndex = 2;
+            // 
+            // lblDay3
+            // 
+            lblDay3.AutoSize = true;
+            lblDay3.BorderStyle = BorderStyle.Fixed3D;
+            lblDay3.Font = new Font("DS-Digital", 27.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblDay3.ForeColor = SystemColors.HighlightText;
+            lblDay3.Location = new Point(293, 225);
+            lblDay3.Name = "lblDay3";
+            lblDay3.Size = new Size(179, 38);
+            lblDay3.TabIndex = 4;
+            lblDay3.Text = "Wednesday";
+            // 
+            // lblDate3
+            // 
+            lblDate3.AutoSize = true;
+            lblDate3.BorderStyle = BorderStyle.Fixed3D;
+            lblDate3.Font = new Font("DS-Digital", 36F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblDate3.ForeColor = SystemColors.HighlightText;
+            lblDate3.Location = new Point(16, 214);
+            lblDate3.Name = "lblDate3";
+            lblDate3.Size = new Size(249, 49);
+            lblDate3.TabIndex = 3;
+            lblDate3.Text = "Feb 12 2025";
+            // 
+            // lblTicks3
+            // 
+            lblTicks3.AutoSize = true;
+            lblTicks3.BorderStyle = BorderStyle.Fixed3D;
+            lblTicks3.Font = new Font("DS-Digital", 36F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblTicks3.ForeColor = SystemColors.HighlightText;
+            lblTicks3.Location = new Point(308, 102);
+            lblTicks3.Name = "lblTicks3";
+            lblTicks3.Size = new Size(70, 49);
+            lblTicks3.TabIndex = 2;
+            lblTicks3.Text = "30";
+            // 
+            // lblTime3
+            // 
+            lblTime3.AutoSize = true;
+            lblTime3.BorderStyle = BorderStyle.Fixed3D;
+            lblTime3.Font = new Font("DS-Digital", 72F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblTime3.ForeColor = SystemColors.HighlightText;
+            lblTime3.Location = new Point(87, 80);
+            lblTime3.Name = "lblTime3";
+            lblTime3.Size = new Size(229, 97);
+            lblTime3.TabIndex = 1;
+            lblTime3.Text = "08:15";
+            // 
+            // lblCity3
+            // 
+            lblCity3.AutoSize = true;
+            lblCity3.Font = new Font("Rockwell", 26.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblCity3.ForeColor = SystemColors.HighlightText;
+            lblCity3.Location = new Point(169, 18);
+            lblCity3.Name = "lblCity3";
+            lblCity3.Size = new Size(117, 39);
+            lblCity3.TabIndex = 0;
+            lblCity3.Text = "Tokyo";
+            // 
+            // panel4
+            // 
+            panel4.BorderStyle = BorderStyle.Fixed3D;
+            panel4.Controls.Add(lblDay4);
+            panel4.Controls.Add(lblDate4);
+            panel4.Controls.Add(lblTicks4);
+            panel4.Controls.Add(lblTime4);
+            panel4.Controls.Add(lblCity4);
+            panel4.Location = new Point(561, 339);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(489, 294);
+            panel4.TabIndex = 3;
+            // 
+            // lblDay4
+            // 
+            lblDay4.AutoSize = true;
+            lblDay4.BorderStyle = BorderStyle.Fixed3D;
+            lblDay4.Font = new Font("DS-Digital", 27.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblDay4.ForeColor = SystemColors.HighlightText;
+            lblDay4.Location = new Point(293, 225);
+            lblDay4.Name = "lblDay4";
+            lblDay4.Size = new Size(179, 38);
+            lblDay4.TabIndex = 4;
+            lblDay4.Text = "Wednesday";
+            // 
+            // lblDate4
+            // 
+            lblDate4.AutoSize = true;
+            lblDate4.BorderStyle = BorderStyle.Fixed3D;
+            lblDate4.Font = new Font("DS-Digital", 36F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblDate4.ForeColor = SystemColors.HighlightText;
+            lblDate4.Location = new Point(16, 214);
+            lblDate4.Name = "lblDate4";
+            lblDate4.Size = new Size(249, 49);
+            lblDate4.TabIndex = 3;
+            lblDate4.Text = "Feb 12 2025";
+            // 
+            // lblTicks4
+            // 
+            lblTicks4.AutoSize = true;
+            lblTicks4.BorderStyle = BorderStyle.Fixed3D;
+            lblTicks4.Font = new Font("DS-Digital", 36F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblTicks4.ForeColor = SystemColors.HighlightText;
+            lblTicks4.Location = new Point(308, 102);
+            lblTicks4.Name = "lblTicks4";
+            lblTicks4.Size = new Size(70, 49);
+            lblTicks4.TabIndex = 2;
+            lblTicks4.Text = "30";
+            // 
+            // lblTime4
+            // 
+            lblTime4.AutoSize = true;
+            lblTime4.BorderStyle = BorderStyle.Fixed3D;
+            lblTime4.Font = new Font("DS-Digital", 72F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblTime4.ForeColor = SystemColors.HighlightText;
+            lblTime4.Location = new Point(87, 80);
+            lblTime4.Name = "lblTime4";
+            lblTime4.Size = new Size(229, 97);
+            lblTime4.TabIndex = 1;
+            lblTime4.Text = "08:15";
+            // 
+            // lblCity4
+            // 
+            lblCity4.AutoSize = true;
+            lblCity4.Font = new Font("Rockwell", 26.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblCity4.ForeColor = SystemColors.HighlightText;
+            lblCity4.Location = new Point(137, 18);
+            lblCity4.Name = "lblCity4";
+            lblCity4.Size = new Size(204, 39);
+            lblCity4.TabIndex = 0;
+            lblCity4.Text = "Amsterdam";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Desktop;
+            ClientSize = new Size(1128, 658);
+            Controls.Add(panel4);
+            Controls.Add(panel3);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Name = "Form1";
+            Text = "What Time Is In...";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            panel4.ResumeLayout(false);
+            panel4.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Label lblCity1;
+        private Label lblTime1;
+        private Label lblTicks1;
+        private Label lblDay1;
+        private Label lblDate1;
+        private Panel panel2;
+        private Label lblDay2;
+        private Label lblDate2;
+        private Label lblTicks2;
+        private Label lblTime2;
+        private Label lblCity2;
+        private Panel panel3;
+        private Label lblDay3;
+        private Label lblDate3;
+        private Label lblTicks3;
+        private Label lblTime3;
+        private Label lblCity3;
+        private Panel panel4;
+        private Label lblDay4;
+        private Label lblDate4;
+        private Label lblTicks4;
+        private Label lblTime4;
+        private Label lblCity4;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace WhatTimeIsIn
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
